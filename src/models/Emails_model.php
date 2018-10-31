773340615253c87983416367875c35f7ce55e642
Brice Pissard <brice@robby.ai>
2018-10-31 13:45:06 +0100

    change

--- src/models/Emails_model.php
+++ src/models/Emails_model.php
@@ -25,7 +25,7 @@ final class Emails_model extends \Stripe\Model
 	 */
 	public static function send( $to, $subject, $message, $from = NULL, $from_name = NULL, $file_name = '', $file_data = NULL, $file_type = 'application/octet-stream' )
 	{
-		if ( class_exists( 'PHPmailer' ) ) {
+		if ( class_exists('PHPmailer') ) {
 			$from 		 = ( ( !isset( $from      ) || empty( $from     ))?'noreply@robby.ai' : $from );
 			$from_name = ( ( !isset( $from_name ) || empty( $from_name))?'noreply@robby.ai' : $from_name );
 
